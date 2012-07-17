1dbd774441f1c09f51e25f65a6945030b4f096a8
sathi <rimli.sathi@gmail.com>
2012-07-17 17:02:44 +0530

    project added

---
+++ demo_sathi/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoSathi::Application.config.secret_token = '09aaa8a5dc890d8bac26a25acddbdde95c42809b8ec5bb815983cf4dce9e82fa3cf494f70b713ace687622f09b48ab1006c1892ba1d33e3a633aa6fe4ccb1863'
